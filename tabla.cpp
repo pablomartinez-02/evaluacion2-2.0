594e51e0b713230e1803de99644592e8b88611e8
pablomartinez-02 <55572515+pablomartinez-02@users.noreply.github.com>
2019-11-29 09:27:53 +0100

    Add files via upload

---
+++ tabla.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(){
+    //Definicion variables
+    int ntabla;
+    char salir;
+    std::cout<<"Que tabla quieres que muestre: ";
+    std::cin >>ntabla;
+    std::cout<<"****************************\n";
+    std::cout<<"*       TABLA DEL ";
+    std::cout<< ntabla;
+    std::cout<<"                *\n";
+    std::cout<<"****************************\n";
+    std::cin >> salir;
+
+}
